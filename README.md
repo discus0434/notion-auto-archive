a7ba600907d35f227a2f94795f3c919095f938cb
discus0434 <discus0434@gmail.com>
2023-01-05 21:02:41 +0000

    add description

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # notion-auto-archive
 
-あとで書く
+## Installation
+
+1. NotionとTwitterのアクセストークンを取得する
+2. Notionにデータベースのページを作成してIDを控える(コネクトも追加しておく）
+3. `git clone`
+4. `docker build` (環境によってCUDAのバージョンは変えたほうがいいかも)
+5. `config/config.py`を参考にしながら`.env`にTwitterとNotionのアクセストークンとかNotionのデータベースIDとかを書く
+6. `config/config.py`の`CANDIDATE_LABELS`を好みで書き換える
+7. `./start_watcher.sh &`
--- config/config.py
+++ config/config.py
@@ -5,24 +5,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Path
-JSON_PATH = Path("notion.json")
-JAVASCRIPT_PATH = Path("js/markdown.js")
-URLS_LOG_PATH = Path("urls.log")
-
+########################################################################
 # Notion
+########################################################################
+# ID of the database to archive
 DATABASE_ID = os.getenv("DATABASE_ID")
+# Access token of the integration (https://www.notion.so/my-integrations)
 NOTION_ACCESS_TOKEN = os.getenv("NOTION_ACCESS_TOKEN")
 
+########################################################################
 # Twitter
+########################################################################
+# Twitter API keys (https://developer.twitter.com/en/portal/dashboard)
+# Make sure to enable "Read, Write, and Direct Messages" permissions
 TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
 TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
 TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
 TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
 TWITTER_TOKEN_SECRET = os.getenv("TWITTER_TOKEN_SECRET")
+# Your Twitter username (without @)
 TWITTER_USER_NAME = os.getenv("TWITTER_USER_NAME")
 
+########################################################################
 # Label Candidates
+########################################################################
+# Labels to be used for the zero-shot-classification model.
+# If there is no label that matches the text, the text will be archived with wrong labels.
+# You can add or remove labels as you like.
 CANDIDATE_LABELS = [
     # AI/ML
     "natural language processing",
@@ -52,3 +61,13 @@ CANDIDATE_LABELS = [
     "lifehack",
     "business",
 ]
+
+########################################################################
+# Path
+########################################################################
+# Path to the directory where the tmp file or log is located
+JSON_PATH = Path("notion.json")
+URLS_LOG_PATH = Path("urls.log")
+
+# Path to the directory where the javascript file is located
+JAVASCRIPT_PATH = Path("js/markdown.js")
