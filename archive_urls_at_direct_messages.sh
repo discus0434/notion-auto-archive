eed05a4d72647bc66b6340be9cb7774c578653c4
discus0434 <discus0434@gmail.com>
2023-01-05 19:15:19 +0000

    update

--- archive_urls_at_direct_messages.sh
+++ archive_urls_at_direct_messages.sh
@@ -3,4 +3,4 @@ export WORKSPACE_DIR=/home/workspace
 cd "${WORKSPACE_DIR}/notion-auto-archive"
 
 source venv/bin/activate
-/usr/bin/python direct_message_watcher.py
+python direct_message_watcher.py
