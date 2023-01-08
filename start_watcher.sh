c50e495fc6831b2d5b451470db79cb9429a5f903
discus0434 <discus0434@gmail.com>
2023-01-08 16:13:51 +0000

    update

--- start_watcher.sh
+++ start_watcher.sh
@@ -3,4 +3,4 @@ export WORKSPACE_DIR=/home/workspace
 cd "${WORKSPACE_DIR}/notion-auto-archive"
 
 source venv/bin/activate
-python direct_message_watcher.py
+CUDA_VISIBLE_DEVICES=1 python direct_message_watcher.py
