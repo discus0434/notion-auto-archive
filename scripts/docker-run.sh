967bc30e6c35532b24a4522d65dd9832ca7e033a
discus0434 <discus0434@gmail.com>
2023-01-25 00:21:44 +0900

    update

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,11 +1,13 @@
-mkdir -p ./engrafo
+PROJECT_DIR="$HOME/notion-auto-archive"
+mkdir -p "$PROJECT_DIR/output"
+
 
 docker run -it \
     --runtime=nvidia \
     --gpus all \
     --env-file ./.env  \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v output:/home/workspace/notion-auto-archive/output \
+    -v $PROJECT_DIR/output:/home/workspace/notion-auto-archive/output \
     -d \
     --restart=always \
     notion-auto-archive:latest /bin/bash
@@ -14,7 +16,8 @@ docker run -it \
     --name pdf-reader \
     --runtime=nvidia \
     --gpus all \
-    -v output:/home/workspace/pdf-reader/output \
+    -v $PROJECT_DIR/pdf-reader:/home/workspace/pdf-reader \
+    -v $PROJECT_DIR/output:/home/workspace/pdf-reader/output \
     -d \
     --restart=always \
     pdf-reader:latest /bin/bash
